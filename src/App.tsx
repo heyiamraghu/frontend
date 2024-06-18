1f70c55f07b28b9b37f57a9e4c71bc8e789ad493
heyiamraghu <hey.iamraghu@gmail.com>
2024-06-19 02:50:38 +0530

    Commented a bunch of codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import './App.css'
 import {NavBar} from "./components/NavBar.tsx";
 import {BlogCard} from "./components/BlogCard.tsx";
-import {BlogModel} from "./models/BlogModel.ts";
-import {useState} from "react";
+// import {BlogModel} from "./models/BlogModel.ts";
+// import {useState} from "react";
 import {useEffect} from "react";
 
 function App() {
-    const [blogs, setBlogs] = useState<BlogModel[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState(null);
+    // const [blogs, setBlogs] = useState<BlogModel[]>([]);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [httpError, setHttpError] = useState(null);
 
     useEffect(() => {
         const fetchBlogs = async () => {
-            const url: string = "https://king-prawn-app-3jqhv.ondigitalocean.app/api/v1/blogs";
+            const url: string = "http://localhost:8080/api/v1/blogs";
             const response = await fetch(url);
             const json = await response.json();
             console.log(json);
         };
         fetchBlogs().catch((error) => {
-            setIsLoading(false);
-            setHttpError(error);
+            // setIsLoading(false);
+            // setHttpError(error);
+            console.log(error);
         })
     }, []);
 
